48ec6708c978a756e4581abeacb05b8b81c6aea3
sonu-shivcharan <sonushivcharan@outlook.com>
2026-01-19 15:25:43 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">test</h1>
+      <Button>test</Button>
+    </div>
+  );
+}
+
+export default App;
